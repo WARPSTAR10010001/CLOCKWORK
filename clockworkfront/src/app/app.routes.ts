84849aa0127de30e349df9d19c1a56404b852a0f
Alpay Gürüz <alpay.gueruez@hsrw.org>
2025-08-13 16:26:17 +0200

    Initial app structure and routing setup
    Added core Angular components for year, month, plan, info, admin, and auth views, along with their templates and styles. Implemented main routing configuration, updated app layout, replaced favicon, and removed placeholder/template files and spec/test configs.

--- clockworkfront/src/app/app.routes.ts
+++ clockworkfront/src/app/app.routes.ts
@@ -1,3 +1,41 @@
 import { Routes } from '@angular/router';
+import { YearComponent } from './year-component/year-component';
+import { InfoComponent } from './info-component/info-component';
+import { MonthComponent } from './month-component/month-component';
+import { PlanComponent } from './plan-component/plan-component';
+import { AdminComponent } from './admin-component/admin-component';
+import { AuthComponent } from './auth-component/auth-component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "",
+        component: YearComponent,
+        title: "Jahresansicht - CLOCKWORK"
+    },
+    {
+        path: "wiki",
+        component: InfoComponent,
+        title: "Wiki - CLOCKWORK"
+
+    },
+    {
+        path: "admin",
+        component: AdminComponent,
+        title: "Adminansicht - CLOCKWORK"
+    },
+    {
+        path: "auth",
+        component: AuthComponent,
+        title: "Anmeldung - CLOCKWORK"
+    },
+    {
+        path: "/:year",
+        component: MonthComponent,
+        title: "Monatsansicht - CLOCKWORK"
+    },
+    {
+        path: "/:year/:month",
+        component: PlanComponent,
+        title: "Dienstplan - CLOCKWORK"
+    }
+];
---
+++ clockworkfront/src/app/year-component/year-component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-year-component',
+  imports: [],
+  templateUrl: './year-component.html',
+  styleUrl: './year-component.css'
+})
+export class YearComponent {
+
+}
