3c15b4ebf7b5669cc360005c0bff4d984bf9ea5b
Alpay Gürüz <alpay.gueruez@hsrw.org>
2025-08-13 13:49:32 +0200

    Change server port to 3000
    Updated the server port from 12345 to 3000 in server.js to use a more conventional development port.

--- clockworkback/server.js
+++ clockworkback/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const session = require("express-session");
 const app = express();
 const { errorLogger, requestLogger } = require("./actionHandler");
-const PORT = 12345;
+const PORT = 3000;
 
 app.use(express.json());
 app.use(session({
