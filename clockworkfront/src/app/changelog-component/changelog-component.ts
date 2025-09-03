44177d8371f895984523170889899fbfffb2cd83
Alpay Gürüz <alpay.gueruez@hsrw.org>
2025-09-03 16:25:59 +0200

    V0.1.1

--- clockworkfront/src/app/changelog-component/changelog-component.ts
+++ clockworkfront/src/app/changelog-component/changelog-component.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-changelog-component',
-  imports: [RouterLink],
+  imports: [],
   templateUrl: './changelog-component.html',
   styleUrl: './changelog-component.css'
 })
