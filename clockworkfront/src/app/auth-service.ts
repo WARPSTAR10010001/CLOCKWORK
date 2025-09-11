90e4ea39f078f377c0d57314387b2a6f8e5a5aab
Alpay Gürüz <alpay.gueruez@hsrw.org>
2025-09-11 15:34:03 +0200

    Update auth-service.ts

--- clockworkfront/src/app/auth-service.ts
+++ clockworkfront/src/app/auth-service.ts
@@ -6,8 +6,8 @@ import { OverlayService } from './overlay-service';
 
 interface AuthStatus {
   loggedIn: boolean;
-  isAdmin: boolean;
-  user?: { id: number; username: string; isAdmin: boolean } | null;
+  role?: 'admin' | 'moderator' | 'default';
+  user?: { id: number; username: string; role: 'admin' | 'moderator' | 'default', department?: string } | null;
   exp?: number;
 }
 
@@ -19,6 +19,9 @@ export class AuthService {
   private adminSubject = new BehaviorSubject<boolean>(false);
   public admin$ = this.adminSubject.asObservable();
 
+  private modSubject = new BehaviorSubject<boolean>(false);
+  public mod$ = this.modSubject.asObservable();
+
   url: string = "http://localhost:3000/api/auth";
 
   constructor(private http: HttpClient, private router: Router, private overlayService: OverlayService) {}
@@ -27,7 +30,10 @@ export class AuthService {
     return this.http.post<AuthStatus>(`${this.url}/login`, { username, password }, { withCredentials: true })
       .pipe(tap(res => {
         this.loggedInSubject.next(res.loggedIn);
-        this.adminSubject.next(!!res.isAdmin);
+
+        const role = res.user?.role;
+        this.adminSubject.next(role === 'admin');
+        this.modSubject.next(role === 'moderator');
       }));
   }
 
@@ -36,6 +42,7 @@ export class AuthService {
       .pipe(tap(() => {
         this.loggedInSubject.next(false);
         this.adminSubject.next(false);
+        this.modSubject.next(false);
         this.overlayService.showOverlay("success", "Erfolgreich abgemeldet.");
         this.router.navigate(['/auth']);
       }));
@@ -47,5 +54,9 @@ export class AuthService {
 
   isAdmin(): boolean {
     return this.adminSubject.value;
+  }
+
+  isMod(): boolean {
+    return this.modSubject.value;
   }
 }
