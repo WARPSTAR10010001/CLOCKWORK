375a3f7f40b906dc9b6cb1975f4a834539c9f82d
Alpay Gürüz <alpay.gueruez@hsrw.org>
2025-09-11 15:19:51 +0200

    V0.1.6 #1
    Moderator component was added since the database roles are finally implemented. The overall html and logic have to be migrated from the admin component. The current admin component will be replaced with a new one that is able to manage and create new tables on the database + users for the departments and optionally also view all available plans

---
+++ clockworkfront/src/app/moderator-component/moderator-component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-moderator-component',
+  imports: [],
+  templateUrl: './moderator-component.html',
+  styleUrl: './moderator-component.css'
+})
+export class ModeratorComponent {
+
+}
