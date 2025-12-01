ee4c5e04b7a675aaa9b0be613277e7a0fd234b08
Alpay Gürüz <alpaygueruez@gmail.com>
2025-12-01 16:42:58 +0100

    V0.5.1 #1

--- clockworkfront/src/app/version-service.ts
+++ clockworkfront/src/app/version-service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 
 @Injectable({ providedIn: 'root' })
 export class VersionService {
-  private version: string = "0.5.0";
+  private version: string = "0.5.1";
 
   getVersion(): string {
     return this.version;
