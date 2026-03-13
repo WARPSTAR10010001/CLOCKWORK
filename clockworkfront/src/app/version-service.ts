fdb06c589cd2bc95f332c86deb3a3a6a06e751b2
WARP ★ <alpaygueruez@gmail.com>
2026-03-13 11:51:47 +0100

    V1.6.1 #1

--- clockworkfront/src/app/version-service.ts
+++ clockworkfront/src/app/version-service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 
 @Injectable({ providedIn: 'root' })
 export class VersionService {
-  private version: string = "1.6.0";
+  private version: string = "1.6.1";
 
   getVersion(): string {
     return this.version;
